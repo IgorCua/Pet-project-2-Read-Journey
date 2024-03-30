52c244729d4b51651e0e88e49987c4ea859168b8
IhorC. <igorchua@gmail.com>
2024-03-30 18:57:41 -0500

    feat: eslint installed and connected; feat: routes connected

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-const path = require('path');
+var path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
+var ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        // test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        // use: 'babel-loader'
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: './public/index.html'
-  //   })
-  // ]
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                // test: /\.(ts|tsx)$/,
+                use: ['babel-loader', 'eslint-loader', 'ts-loader'],
+                // use: 'babel-loader'
+                exclude: /node_modules/,
+                // loader: 'eslint-loader',
+                options: {
+                    rules: {
+                        indent: ['error', 4]
+                        // indent: ["error", { "allowIndentationTabs": true }]
+                    }
+                }
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new ESLintPlugin(),
+        //   new HtmlWebpackPlugin({
+        //     template: './public/index.html'
+        //   })
+    ]
 };
