8757c92200ebe60f350fc8ca744dbc3dbdb53560
IhorC. <igorchua@gmail.com>
2024-05-28 10:11:45 -0500

    chore: token assignment to axios torned on;

--- src/services/axiosSettings.ts
+++ src/services/axiosSettings.ts
@@ -3,20 +3,20 @@ import localStorage from "redux-persist/es/storage";
 
 export const axiosToken = {
     set(token?: string) {
-        // if(token) {
-        //     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-        // } 
-        // else {
-        //     localStorage.getItem('persist:auth').then(data => {
-        //         // if(typeof data === 'string'){
-        //             const storageToken = typeof data === 'string' && JSON.parse(JSON.parse(data).token);
-        //         // }
-        //         // const storageToken = JSON.parse(JSON.parse(data).token);
-        //         if(storageToken) axios.defaults.headers.common.Authorization = `Bearer ${storageToken}`;
+        if(token) {
+            axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+        } 
+        else {
+            localStorage.getItem('persist:auth').then(data => {
+                // if(typeof data === 'string'){
+                    const storageToken = typeof data === 'string' && JSON.parse(JSON.parse(data).token);
+                // }
+                // const storageToken = JSON.parse(JSON.parse(data).token);
+                if(storageToken) axios.defaults.headers.common.Authorization = `Bearer ${storageToken}`;
 
-        //         return;
-        //     });
-        // }
+                return;
+            });
+        }
     },
     unset() {
       axios.defaults.headers.common.Authorization = ``;
