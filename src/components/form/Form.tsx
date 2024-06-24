d603ad36723eaaf784145d3c77130791a80e98a1
IhorC. <igorchua@gmail.com>
2024-06-24 17:38:38 -0500

    fix: password eye rerendered form on klick;

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -25,9 +25,7 @@ type Props = {
     fieldsType?: {} 
 }
 
-export const Form = (props: Props) => {
-    // const { touched, errors, isSubmitting, message } = props;
-    // const { touched, errors, isSubmitting } = props;
+const InnerForm = (props: Props & FormikProps<any>) => { 
     const {
         initialValues, 
         validationSchema, 
@@ -37,7 +35,21 @@ export const Form = (props: Props) => {
         sx,
         children
     } = props;
+    console.log(props)
+    const { touched, errors, isSubmitting } = props;
+    const touchedKeysArr = Object.keys(touched);
+    const errorsKeysArr = Object.keys(errors);
+    
     const [isEyeOpen, setIsEyeOpen] = useState(false);
+    // console.log('touchedKeysArr', touchedKeysArr);
+    // console.log('errors array', errorsKeysArr);
+
+    const checkIfError = (name: string) => {
+        if(touchedKeysArr.length > 0){
+            return (touchedKeysArr.includes(name) && errorsKeysArr.includes(name)) ? true : false;
+        }
+    }
+    // if(errors) console.log('errors', errors);
 
     const renderEyeIcon = () => {
         // event?: React.MouseEvent<HTMLOrSVGElement>
@@ -73,108 +85,136 @@ export const Form = (props: Props) => {
         )
     }
 
-    const InnerForm = (props: FormikProps<any>) => { 
-        const { touched, errors, isSubmitting } = props;
-        const touchedKeysArr = Object.keys(touched);
-        const errorsKeysArr = Object.keys(errors);
+    return <CustomForm sx={sx}>
+        {inputsDataArr.length > 0 && inputsDataArr.map((obj, i) => {
+            return <InputContainer key={i}>{obj.type !== 'password' 
+                ? <>
+                    <Field 
+                        type={obj.type} 
+                        name={obj.name} 
+                        component={FormTextField}
+                        InputProps={{
+                            startAdornment:
+                                <InputAdornment 
+                                    position="start"
+                                >{`${obj.placeholder}`}</InputAdornment>,
+                        }}
+                        sx={{
+                            zIndex: '10',
 
-        console.log('touchedKeysArr', touchedKeysArr);
-        console.log('errors array', errorsKeysArr);
+                            '&:hover .MuiOutlinedInput-notchedOutline': {
+                                borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
+                            },
+                            '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                border: `${checkIfError(obj.name) && '1px solid transparent'}`,
+                            },
+                        }}
+                    />
+                    <CustomErrorMessage name={obj.name} component='p'/>
+                    {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
+                </>
+                : <>
+                    <Field 
+                        type={!isEyeOpen ? 'password' : 'text'} 
+                        name={obj.name} 
+                        component={FormTextField}
+                        InputProps={{
+                            startAdornment:
+                                <InputAdornment 
+                                    position="start"
+                                >{`${obj.placeholder}`}</InputAdornment>,
+                            endAdornment: (renderEyeIcon())
+                        }}
+                        sx={{
+                            zIndex: '10',
 
-        const checkIfError = (name: string) => {
-            if(touchedKeysArr.length > 0){
-                return (touchedKeysArr.includes(name) && errorsKeysArr.includes(name)) ? true : false;
+                            '&:hover .MuiOutlinedInput-notchedOutline': {
+                                borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
+                            },
+                            '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                border: `${checkIfError(obj.name) && '1px solid transparent'}`,
+                            },
+                        }}
+                    />
+                    <CustomErrorMessage name={obj.name} component='p'/>
+                    {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
+                </>
             }
-        }
-        // if(errors) console.log('errors', errors);
+            </InputContainer>
+        })}
+        <ButtonBox>
+            <Submit id="submitButton" type="submit">
+                {submitName}
+            </Submit>
+            {children}
+        </ButtonBox>
+    </CustomForm>
+}
 
-        return <CustomForm sx={sx}>
-            {inputsDataArr.length > 0 && inputsDataArr.map((obj, i) => {
-                return <InputContainer key={i}>{obj.type !== 'password' 
-                    ? <>
-                        <Field 
-                            type={obj.type} 
-                            name={obj.name} 
-                            component={FormTextField}
-                            InputProps={{
-                                startAdornment:
-                                    <InputAdornment 
-                                        position="start"
-                                    >{`${obj.placeholder}`}</InputAdornment>,
-                            }}
-                            sx={{
-                                zIndex: '10',
+export const Form = (props: Props) => {
+    // const { touched, errors, isSubmitting, message } = props;
+    // const { touched, errors, isSubmitting } = props;
+    const {
+        initialValues, 
+        validationSchema, 
+        inputsDataArr, 
+        handleSubmit, 
+        submitName, 
+        sx,
+        children
+    } = props;
+    // const [isEyeOpen, setIsEyeOpen] = useState(false);
 
-                                '&:hover .MuiOutlinedInput-notchedOutline': {
-                                    borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
-                                },
-                                '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                    border: `${checkIfError(obj.name) && '1px solid transparent'}`,
-                                },
-                            }}
-                        />
-                        <CustomErrorMessage name={obj.name} component='p'/>
-                        {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
-                    </>
-                    : <>
-                        <Field 
-                            type={!isEyeOpen ? 'password' : 'text'} 
-                            name={obj.name} 
-                            component={FormTextField}
-                            InputProps={{
-                                startAdornment:
-                                    <InputAdornment 
-                                        position="start"
-                                    >{`${obj.placeholder}`}</InputAdornment>,
-                                endAdornment: (renderEyeIcon())
-                            }}
-                            sx={{
-                                zIndex: '10',
+    // const renderEyeIcon = () => {
+    //     // event?: React.MouseEvent<HTMLOrSVGElement>
+    //     return (isEyeOpen 
+    //         ? <InputAdornment position='end' onClick={() => setIsEyeOpen(false)}>
+    //             <Icon 
+    //                 iconName={'#icon-eye-opened'} 
+    //                 sx={{
+    //                     width: '15px', 
+    //                     height: '15px', 
+    //                     cursor: 'pointer', 
+    //                     [theme.breakpoints.up('tablet')]:{
+    //                         width: '20px', height: '20px'
+    //                     }}}
+    //                 aria-label="hide password"
+    //                 onClick={() => setIsEyeOpen(false)}
+    //             />
+    //           </InputAdornment>
+    //         : <InputAdornment position='end' onClick={() => setIsEyeOpen(true)}>
+    //             <Icon 
+    //                 iconName={'#icon-eye-closed'} 
+    //                 sx={{
+    //                     width: '15px', 
+    //                     height: '15px', 
+    //                     cursor: 'pointer', 
+    //                     [theme.breakpoints.up('tablet')]:{
+    //                         width: '20px', height: '20px'
+    //                     }}}
+    //                 aria-label="show password"
+    //                 onClick={() => setIsEyeOpen(true)}
+    //             />
+    //           </InputAdornment>   
+    //     )
+    // }
 
-                                '&:hover .MuiOutlinedInput-notchedOutline': {
-                                    borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
-                                },
-                                '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                    border: `${checkIfError(obj.name) && '1px solid transparent'}`,
-                                },
-                            }}
-                        />
-                        <CustomErrorMessage name={obj.name} component='p'/>
-                        {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
-                    </>
-                }
-                </InputContainer>
-            })}
-            <ButtonBox>
-                <Submit id="submitButton" type="submit">
-                    {submitName}
-                </Submit>
-                {children}
-            </ButtonBox>
-        </CustomForm>
-    }
+    // const InnerForm = (props: Props & FormikProps<any>) => { 
+    //     const { touched, errors, isSubmitting } = props;
+    //     const touchedKeysArr = Object.keys(touched);
+    //     const errorsKeysArr = Object.keys(errors);
 
-    const MyForm = withFormik<any, any>({
-        // Transform outer props into form values
-        mapPropsToValues: () => ({...initialValues}),
-      
-        // Add a custom validation function (this can be async too!)
-        validationSchema: validationSchema,
-      
-        handleSubmit: values => {
-          // do submitting things
-          return values;
-        },
-      })(InnerForm);
+    //     // console.log('touchedKeysArr', touchedKeysArr);
+    //     // console.log('errors array', errorsKeysArr);
 
-    return <MyForm/>
+    //     const checkIfError = (name: string) => {
+    //         if(touchedKeysArr.length > 0){
+    //             return (touchedKeysArr.includes(name) && errorsKeysArr.includes(name)) ? true : false;
+    //         }
+    //     }
+    //     // if(errors) console.log('errors', errors);
 
-    // return <Formik
-    //     initialValues={initialValues}
-    //     validationSchema={validationSchema}
-    //     onSubmit={handleSubmit}
-    // > 
-    //     <CustomForm sx={sx}>
+    //     return <CustomForm sx={sx}>
     //         {inputsDataArr.length > 0 && inputsDataArr.map((obj, i) => {
     //             return <InputContainer key={i}>{obj.type !== 'password' 
     //                 ? <>
@@ -188,9 +228,19 @@ export const Form = (props: Props) => {
     //                                     position="start"
     //                                 >{`${obj.placeholder}`}</InputAdornment>,
     //                         }}
+    //                         sx={{
+    //                             zIndex: '10',
+
+    //                             '&:hover .MuiOutlinedInput-notchedOutline': {
+    //                                 borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
+    //                             },
+    //                             '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+    //                                 border: `${checkIfError(obj.name) && '1px solid transparent'}`,
+    //                             },
+    //                         }}
     //                     />
     //                     <CustomErrorMessage name={obj.name} component='p'/>
-    //                     {/* <ErrorBox name={obj.name}/> */}
+    //                     {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
     //                 </>
     //                 : <>
     //                     <Field 
@@ -204,8 +254,19 @@ export const Form = (props: Props) => {
     //                                 >{`${obj.placeholder}`}</InputAdornment>,
     //                             endAdornment: (renderEyeIcon())
     //                         }}
+    //                         sx={{
+    //                             zIndex: '10',
+
+    //                             '&:hover .MuiOutlinedInput-notchedOutline': {
+    //                                 borderColor: `${checkIfError(obj.name) ? 'transparent' : theme.palette.custom.authInputBorder}`
+    //                             },
+    //                             '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+    //                                 border: `${checkIfError(obj.name) && '1px solid transparent'}`,
+    //                             },
+    //                         }}
     //                     />
     //                     <CustomErrorMessage name={obj.name} component='p'/>
+    //                     {checkIfError(obj.name) && <ErrorBox name={obj.name}/>}
     //                 </>
     //             }
     //             </InputContainer>
@@ -217,5 +278,24 @@ export const Form = (props: Props) => {
     //             {children}
     //         </ButtonBox>
     //     </CustomForm>
-    // </Formik>
+    // }
+
+    const MyForm = withFormik<any, any>({
+        // Transform outer props into form values
+        mapPropsToValues: () => ({...initialValues}),
+      
+        // Add a custom validation function (this can be async too!)
+        validationSchema: validationSchema,
+      
+        handleSubmit: values => {
+            handleSubmit(values)
+        //   return values;
+        },
+    })(InnerForm)
+    return <MyForm 
+        inputsDataArr={inputsDataArr} 
+        submitName={submitName} 
+        children={children} 
+        sx={sx}
+    />
 }
